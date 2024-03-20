f7f3098179ea0aac8a60de2e239bfe3f17edb999
ghbouzrbay <atighmarcelline@gmail.com>
2024-03-20 12:03:48 +0000

    added the frontEnd of App

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// React component for EasyBus
+class EasyBusApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // Define state variables here
+        };
+    }
+
+    render() {
+        return (
+            <div className="easybus-app">
+                {/* Your JSX for EasyBus */}
+            </div>
+        );
+    }
+}
+
+// Render EasyBusApp component to the DOM
+ReactDOM.render(<EasyBusApp />, document.getElementById('root'));
